73385960d5495c00200b6216a1602611b8fcca5c
Ruargh <ruargh@gmail.com>
2019-01-02 16:55:55 +0200

    Refactor: Get image height and width for book thumbnail

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # MyReads Project
 
-The MyReads projects  allows you to track books by adding them to shelves. The available shelves are Currently Reading, Want to Read and Read. You can use the Search facillity to search for books by author or title and then add them to the shelves. To remove a book from the library set it's shelf to "None".
+The MyReads projects allows you to track books by adding them to shelves. The available shelves are Currently Reading, Want to Read and Read. You can use the Search facillity to search for books by author or title and then add them to the shelves. To remove a book from the library set it's shelf to "None".
 
 ## TL;DR
 
 To run the project:
 
 * install all project dependencies with `npm install`
-* start the development server with `npm start`
+* start the server with `npm start`
 
 
 ## Important
@@ -15,4 +15,4 @@ The backend API uses a fixed set of cached search results and is limited to a pa
 
 ## Create React App
 
-This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app). You can find more information on how to perform common tasks [here](https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md).
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
--- src/components/BooksGrid/Book/BookTop/BookCover.js
+++ src/components/BooksGrid/Book/BookTop/BookCover.js
@@ -4,29 +4,40 @@ import React, { Component } from 'react';
 export default class BookCover extends Component {
 
 
-  // getImageHeight = (url) => {
-  //   const image = new Image();
-  //   image.src = url;
-  //   return image.naturalHeight
-  // }
+  getImageHeight = (url) => {
+    const image = new Image();
+    image.src = url;
+    return image.naturalHeight
+  }
+
+  getImageDimensions = (url) => {
+    const image = new Image();
+    let imageDimensions = [];
+    image.src = url;
+    imageDimensions = [image.naturalWidth, image.naturalHeight]
+    return imageDimensions;
+  }
 
   render() {
     const bookThumbnailURL = this.props.bookThumbnailURL;
-    //const getImageHeight = this.getImageHeight
+    const getImageHeight = this.getImageHeight
+    const imageDimensions = this.getImageDimensions(bookThumbnailURL)
 
     return (
       
       <div
         className="book-cover"
         style={{
-          width: 128,
-          height: 190, //getImageHeight(bookThumbnailURL),
+          //width: 128,
+          width: imageDimensions[0],
+          //height: 190,
+          height: imageDimensions[1],
           maxHeight: 200,
           backgroundImage: `url("${bookThumbnailURL}")`,
           backgroundSize: "contain",
           backgroundRepeat: "no-repeat",
-          backgroundPosition: "center",
-          backgroundColor: "black",
+          backgroundPosition: "center bottom",
+          backgroundColor: "white",
         }}
       />
     );
